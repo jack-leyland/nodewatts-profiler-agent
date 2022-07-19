5e0e6379e1c170cb3f24761715f55b0ea9ecc4cb
jack-leyland <jackjleyland@gmail.com>
2022-07-19 11:29:58 +0100

    add es6 imports option

---
+++ src/es6-imports.js
@@ -0,0 +1,10 @@
+import { createRequire } from "module";
+const NWrequire = createRequire(import.meta.url);
+var nodeWattsZmq = NWrequire("zeromq");
+const nodeWattsV8Profiler = NWrequire('v8-profiler-next');
+const nodeWattsFs = NWrequire("fs");
+var nodeWattsSaveToDB = require(process.env.PATH_TO_DB_SERVICE).ingestFile;
+const nodeWattsTitle = String(process.env.PROFILE_TITLE);
+const nodeWattsPort = String(process.env.TEST_SOCKET_PORT);
+const nodeWattsPath = String(process.env.NODEWATTS_TMP_PATH);
+v8Profiler.setGenerateType(1);
