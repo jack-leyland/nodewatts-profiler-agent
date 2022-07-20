4a91d604d6ddc405430fc0b806c7a8d4f13f4062
jack-leyland <jackjleyland@gmail.com>
2022-07-20 14:23:56 +0100

    fix zmq import and test args

--- src/test-runner.js
+++ src/test-runner.js
@@ -1,10 +1,15 @@
 const port = process.env.TEST_SOCKET_PORT; // Nodewatts processs will inject these vars
-const testCmdArgs = process.env.TESTCMD.split(' ')
-var zmq = require("nw-zeromq");
+const testCmd = process.env.TESTCMD.split(' ')
+const zmqModule = process.env.ZMQ_INSTALLED_PATH
+var zmq = require(zmqModule);
 const { spawn } = require('child_process');
 const events = require('events');
 const { exit } = require("process");
 const emitter = new events.EventEmitter();
+
+var cmd = testCmd[0]
+testCmd.splice(0,1)
+var args = testCmd
 
 async function testRunner() {
   const sock = new zmq.Request();
@@ -12,7 +17,7 @@ async function testRunner() {
   await sock.send("start");
   const [res] = await sock.receive();
   if (res.toString() === "start-success") {
-    testProc = spawn('npm', testCmdArgs);
+    testProc = spawn(cmd, args);
     testProc.on('exit', (exitCode) => {
       if (parseInt(exitCode) !== 0) {
           console.log(exitCode)
@@ -22,7 +27,7 @@ async function testRunner() {
     emitter.on('testExit', async function() {
       await sock.send("stop")
       const [res] = await sock.receive();
-      if (res.toString() === "stop-success") exit();
+      if (res.toString() === "stop-success") exit(0);
     })
   }
 }
